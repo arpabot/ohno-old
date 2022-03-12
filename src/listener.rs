266bc63ef0170a67efd02eb03c04e92d397ce344
Sueqkjs <sueqk@outlook.jp>
2022-03-12 01:41:14 +0000

    clippy

--- src/listener.rs
+++ src/listener.rs
@@ -1,6 +1,7 @@
 use crate::{Data, Error};
 use poise::serenity_prelude as serenity;
 use poise::serenity_prelude::model::{gateway::Activity, user::OnlineStatus};
+use regex::Regex;
 
 pub async fn event_listener(
   ctx: &serenity::Context,
@@ -14,10 +15,11 @@ pub async fn event_listener(
       ctx.set_presence(Some(Activity::listening("Your Voice 24/7")), OnlineStatus::DoNotDisturb).await;
     }
     poise::Event::Message { new_message: msg } => {
+      let re = Regex::new(r#"https?://[\w/:%#$&?()~.=+-]+"#)?;
       let key: u64 = msg.guild_id.unwrap().into();
-      if user_data.queues.lock().await.contains_key(&key) {
-        if user_data.queues.lock().await.get(&key).unwrap().channel_id
-          == msg.channel(&ctx.http).await?.id()
+  
+        if user_data.queues.lock().await.contains_key(&key) && (user_data.queues.lock().await.get(&key).unwrap().channel_id
+          == msg.channel(&ctx.http).await?.id())
         {
           user_data
             .queues
@@ -25,9 +27,8 @@ pub async fn event_listener(
             .await
             .get(&key)
             .unwrap()
-            .play(msg.content.clone())
+            .play(re.replace_all(&msg.content_safe(&ctx.cache), "URL省略").chars().take(500).collect::<String>())
             .await;
-        }
       }
     }
     _ => (),
